5c94666276e0c6e832802853db73dbfda3f61751
samirvithlani <samir.vithlani83955@gmail.com>
2025-04-01 18:48:54 +0530

    operaotrs

---
+++ operators/arithmetic1.py
@@ -0,0 +1,21 @@
+no1 = int(input("enter no1"))
+no2 = int(input("enter no2"))
+
+ans = no1 + no2
+print("sum = ",ans)
+
+ans = no1 - no2
+print("sub = ",ans)
+
+
+ans = no1 * no2
+print("mul = ",ans)
+
+ans = no1 / no2 #float
+print("div = ",ans)
+
+ans = no1 // no2
+print("div int = ",ans)
+
+ans = no1 ** no2
+print("pow = ",ans)
---
+++ operators/asignmentop1.py
@@ -0,0 +1,16 @@
+# = : right side valud left side variable asign
+
+x =10
+x+=1
+print(x)
+x-=1
+print(x)
+x*=1
+print(x)
+x/=1
+print(x)
+
+'''
+    multiline comment 
+    python has not increment decrement operators....
+'''
