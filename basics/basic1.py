28e2fd41b6f69ef5e65f4375fb00b3fe656296e2
samirvithlani <samir.vithlani83955@gmail.com>
2025-04-01 18:23:32 +0530

    python start

---
+++ basics/basic1.py
@@ -0,0 +1,22 @@
+print("Hello all") #print function
+
+#variable... // single line comment #
+x = 100 #int
+print(x)
+print("value of x = ",x)
+print("type of x = ",type(x))
+
+pers = 78.89
+print("value of pers = ",pers)
+print("type of pers = ",type(pers))
+
+user = "Ram"
+print("value of user = ",user)
+print("type of users = ",type(user))
+
+#void
+
+data = None
+print("value of data = ",None)
+print("type of data = ",type(None))
+
---
+++ basics/basic2.py
@@ -0,0 +1,10 @@
+#scanf("%d",&akjns) input
+#input --> param return str
+#no = input("enter no")
+no = int(input("enter no")) #type casting...
+print("value of no = ",no+1) #"100"
+print("type of no = ",type(no))
+
+
+pers = float(input("enter pers"))
+print("value of pers = ",pers)
