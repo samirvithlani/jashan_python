0d7948fdef33c06d2c3a2dd4c0bbebc9c1609e75
samirvithlani <samir.vithlani83955@gmail.com>
2025-11-04 15:28:22 +0530

    set

---
+++ ds/set/setDemo1.py
@@ -0,0 +1,21 @@
+# data = set({})
+# print(data)
+
+data = {100,20,89,34,56,78,90,10,100,34}
+print(data)
+#print(type(data))
+
+data.add(77)
+print(data)
+
+removedElm = data.pop()
+print("removing...",removedElm)
+print(data)
+if 566 in data:
+    data.remove(566) #key error
+else:
+    print("566 is not found")    
+print(data)
+
+data.discard(20)
+print(data)
